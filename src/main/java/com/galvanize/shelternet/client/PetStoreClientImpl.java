264969d4c6fb0f694c5569bb49300247326ef893
smlhbt88 <samuel.habte@cognizant.com>
2021-02-24 14:55:50 -0600

    added basic auth when calling pet store

--- src/main/java/com/galvanize/shelternet/client/PetStoreClientImpl.java
+++ src/main/java/com/galvanize/shelternet/client/PetStoreClientImpl.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,16 +19,20 @@ import java.util.stream.Collectors;
 public class PetStoreClientImpl implements PetStoreClient {
     @Value("${petstore.url}")
     private String petStoreUrl;
+    @Value("${petstore.username}")
+    private String petStoreUsername;
+    @Value("${petstore.password}")
+    private String petStorePassword;
 
     @Override
     public List<AnimalReturnFromPetStoreDto> returnRequest(AnimalRequestIds animalRequestIds) {
-        petStoreUrl += "/animals/return-request";
         RestTemplate restTemplate = new RestTemplate();
-
+        restTemplate.getInterceptors().add(
+                new BasicAuthorizationInterceptor(petStoreUsername, petStorePassword));
         List<String> animalIds = animalRequestIds.getAnimalIds().stream().map(Object::toString).collect(Collectors.toList());
         HttpEntity<List<String>> request = new HttpEntity<>(animalIds);
         ParameterizedTypeReference<List<AnimalReturnFromPetStoreDto>> responseType = new ParameterizedTypeReference<>() {
         };
-        return restTemplate.exchange(petStoreUrl, HttpMethod.DELETE, request, responseType).getBody();
+        return restTemplate.exchange(petStoreUrl+"/animals/return-request", HttpMethod.DELETE, request, responseType).getBody();
     }
 }
