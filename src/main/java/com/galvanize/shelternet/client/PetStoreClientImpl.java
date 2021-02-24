dd59d5faf683c3d81f4f764f905815a611ceb90f
Samuel Horga <samyhorga@yahoo.com>
2021-02-24 10:39:21 -0700

    Extracted pet store url to properties files.

--- src/main/java/com/galvanize/shelternet/client/PetStoreClientImpl.java
+++ src/main/java/com/galvanize/shelternet/client/PetStoreClientImpl.java
@@ -2,6 +2,7 @@ package com.galvanize.shelternet.client;
 
 import com.galvanize.shelternet.model.AnimalRequestIds;
 import com.galvanize.shelternet.model.AnimalReturnFromPetStoreDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -15,10 +16,12 @@ import java.util.stream.Collectors;
 @Component
 @Profile("!test")
 public class PetStoreClientImpl implements PetStoreClient {
+    @Value("${petstore.url}")
+    private String petStoreUrl;
 
     @Override
     public List<AnimalReturnFromPetStoreDto> returnRequest(AnimalRequestIds animalRequestIds) {
-        String petStoreUrl = "https://pet-store-api-staging.herokuapp.com/animals/return-request";
+        petStoreUrl += "/animals/return-request";
         RestTemplate restTemplate = new RestTemplate();
 
         List<String> animalIds = animalRequestIds.getAnimalIds().stream().map(Object::toString).collect(Collectors.toList());
